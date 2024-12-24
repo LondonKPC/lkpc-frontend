76b583487169081288bf2be38bee1890c41e0ca4
Eunsung Kim <jeromekim0503@gmail.com>
2024-12-24 16:35:43 -0500

    add button hover feature

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Footer from "@/components/Footer";
 import GridButton from "@/components/GridButton";
+import "./home.css";
 
 function Home() {
   return (
     <div className="h-full w-full flex items-center justify-center bg-gray-300">
-      <div className="grid grid-cols-3 gap-4 h-[500px] w-[500px]">
-        <GridButton imageSrc={require("@/images/grid/box1.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box2.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box3.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box4.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box5.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box6.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box7.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box8.jpg").default} />
-        <GridButton imageSrc={require("@/images/grid/box9.jpg").default} />
+      <div className="home-grid">
+        <GridButton imageSrc={require("@/images/grid/box1.jpg").default} text="About"/>
+        <GridButton imageSrc={require("@/images/grid/box2.jpg").default} text="Group"/>
+        <GridButton imageSrc={require("@/images/grid/box3.jpg").default} text="School"/>
+        <GridButton imageSrc={require("@/images/grid/box4.jpg").default} text="News"/>
+        <GridButton imageSrc={require("@/images/grid/box5.jpg").default} text="Ministry"/>
+        <GridButton imageSrc={require("@/images/grid/box6.jpg").default} text="Family"/>
+        <GridButton imageSrc={require("@/images/grid/box7.jpg").default} text="Offering"/>
+        <GridButton imageSrc={require("@/images/grid/box8.jpg").default} text="Events" />
+        <GridButton imageSrc={require("@/images/grid/box9.jpg").default} text="Contact"/>
       </div>
     </div>
   );
 }
+
 
 export default Home;
--- components/GridButton.tsx
+++ components/GridButton.tsx
@@ -3,15 +3,20 @@ import React from "react";
 
 interface GridButtonProps {
   imageSrc: StaticImageData;
+  text: string;
 }
-function GridButton({ imageSrc }: GridButtonProps) {
+function GridButton({ imageSrc, text }: GridButtonProps) {
   return (
-    <div className="bg-red-500 flex items-center justify-center">
+    <div className="relative bg-transparent flex items-center justify-center transition duration-300 ease-in-out hover:scale-110">
       <img
         src={imageSrc.src}
         alt="Grid Button"
-        className="h-full w-full object-cover"
+        className="rounded-full h-full w-full object-cover border-solid border-2"
       />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <span className="text-white text-xs md:text-sm lg:text-lg 2xl:text-4xl font-bold font-sans ">{text}</span>
+      </div>
+
     </div>
   );
 }
