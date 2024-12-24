a49ca2b5a0f2aa677956e58492f4ba904f8fa696
Eunsung Kim <jeromekim0503@gmail.com>
2024-12-24 13:51:56 -0500

    add grid button images

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import capy from "@/images/capy.png";
 import Footer from "@/components/Footer";
 import GridButton from "@/components/GridButton";
 
 function Home() {
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className="h-full w-full flex items-center justify-center bg-gray-300">
       <div className="grid grid-cols-3 gap-4 h-[500px] w-[500px]">
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
-        <GridButton imageSrc={capy} />
+        <GridButton imageSrc={require("@/images/grid/box1.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box2.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box3.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box4.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box5.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box6.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box7.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box8.jpg").default} />
+        <GridButton imageSrc={require("@/images/grid/box9.jpg").default} />
       </div>
     </div>
   );
