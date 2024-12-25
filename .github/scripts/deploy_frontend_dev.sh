1eefcc87e5d66e6c5ec6813f73ab90134b80c8b1
Brian Park <brianpark1101@gmail.com>
2024-12-25 00:13:14 -0500

    testing

--- .github/scripts/deploy_frontend_dev.sh
+++ .github/scripts/deploy_frontend_dev.sh
@@ -5,7 +5,7 @@
 if [ "$BRANCH_NAME" == "dev" ]; then
     echo "Deploying to Development..."
     # Sync to dev S3 bucket
-    aws s3 sync out/ s3://lkpc-hosting-dev --delete
+
 
 else
     echo "Branch is not configured for deployment. Exiting..."
