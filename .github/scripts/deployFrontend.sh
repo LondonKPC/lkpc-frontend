09d523157f450e614c7a56bcbfcc94ebe4dbaf62
Brian Park <brianpark1101@gmail.com>
2024-12-24 22:56:52 -0500

    updated workflow

--- .github/scripts/deployFrontend.sh
+++ .github/scripts/deployFrontend.sh
@@ -1,6 +1,5 @@
-# deployFrontend.sh
-
 #!/bin/bash
+# deployFrontend.sh
 
 # Gets the branch name from the GitHub reference (ex. refs/head/main)
 BRANCH_NAME="${GITHUB_REF##*/}"
@@ -8,12 +7,12 @@ BRANCH_NAME="${GITHUB_REF##*/}"
 if [ "$BRANCH_NAME" == "main" ]; then
     echo "Deploying to Production..."
     # Sync to production S3 bucket
-    aws s3 sync ./out/ s3://lkpc-hosting-prod --delete
+    aws s3 sync out/ s3://lkpc-hosting-prod --delete
 
 elif [ "$BRANCH_NAME" == "dev" ]; then
     echo "Deploying to Development..."
     # Sync to dev S3 bucket
-    aws s3 sync ./out/ s3://lkpc-hosting-dev --delete
+    aws s3 sync out/ s3://lkpc-hosting-dev --delete
 
 else
     echo "Branch is not configured for deployment. Exiting..."
