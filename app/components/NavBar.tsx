6ce0cb3dfd2ff19f83183ec1a0eca3656e7d927c
Zusty. <isaachj06@gmail.com>
2024-12-26 13:09:27 -0500

    Navigation bar with mostly working mobie capabilit
    If the user has the menu open. then goes full screen, the menu will still be open

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,24 +6,29 @@ import "./NavBar.css";
 
 export default function NavBar() {
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false); 
+
+    const handleToggle = () => { 
+        setIsOpen(!isOpen); 
+    };
 
     return (
             <div className="w-full h-full bg-transparent">
-                    <nav className="flex justify-between items-center text-white py-2 px-4 md:px-8 bg-transparent drop-shadow-md relative">
-                        {/* Desktop Menu */}
-                        <a href="/home" className="flex-shrink-0"> 
-                            <img src={LKPC_Logo.src} alt="LKPC Logo" className="w-24 h-24 hover:scale-105 transition-all" />
-                        </a>
+                {/* Desktop Menu */}
+                <a href="/home" className="flex-shrink-0 fixed"> 
+                    <img src={LKPC_Logo.src} alt="LKPC Logo" className="w-24 h-24 hover:scale-105 transition-all" />
+                </a>
+                <nav className="flex items-center text-white py-2 px-4 md:px-8 bg-transparent drop-shadow-md relative">
 
+                    <div className="w-full">
                         {/* Desktop Menu */}
-                        <div className="flex justify-center">
-                            <ul className="hidden md:flex flex-1 items-center gap-12 font-semibold text-base">
+                        <div className="flex justify-center items-center">
+                            <ul className="hidden md:flex flex-1 justify-center items-center gap-12 font-semibold text-base">
                                 <li className="navbar-link">
                                     <a href="/about" className="navbar-link-text">About</a>
                                 </li>
                                 <li className="navbar-link">
-                                    <a href="/service" className="navbar-link-text">Service</a>
+                                    <a href="/connect" className="navbar-link-text">Connect</a>
                                 </li>
                                 <li className="navbar-link">
                                     <a href="/event" className="navbar-link-text">Events</a>
@@ -33,67 +38,51 @@ export default function NavBar() {
                                 </li>
                             </ul>
                         </div>
-                        {/* Hamburger Icon (only visible on smaller screens) not working */}
-                        
-                        <button onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Toggle Menu">
-                            {isMenuOpen ? (
-                            // Close Icon
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="black"
-                                className="md:hidden size-16"
-                            >
-                                <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                            ) : (
-                            // Hamburger Icon
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="black"
-                                className="md:hidden size-16"
-                            >
-                                <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                />
-                            </svg>
-                            )}
-                        </button>
+                    </div>
 
-                        {/* Mobile Menu */}
-                        <div className={`absolute md:hidden top-28 left-0 w-full bg-black 
-                            flex flex-col items-center gap-6 font-semibold text-lg rounded-lg
-                            transition-transform ${isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"} 
-                            transform duration-300 ease-in-out`}>
+                    <button onClick={handleToggle} className="md:hidden text-3xl p-2 focus:outline-none" > 
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="black"
+                        className="size-16"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                            />
+                        </svg>
+                    </button>
+                </nav> 
 
-                            
-                            <ul className="p-0 m-0 list-none text-center">
-                                <li className="mobile-navbar-link">
-                                    <a href="/about">About</a>
-                                </li>
-                                <li className="mobile-navbar-link">
-                                    <a href="/service">Service</a>
-                                </li>
-                                <li className="mobile-navbar-link">
-                                    <a href="/event">Events</a>
-                                </li>
-                                <li className="mobile-navbar-link">
-                                    <a href="/contact">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
+                {/* Mobile Menu */} 
+                <div className={`md:hidden fixed top-0 right-0 w-64 h-full bg-gray-800 text-white transition-transform transform ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                    <button onClick={handleToggle} className="absolute top-4 left-4 text-2xl focus:outline-none" >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="white"
+                        className="size-16"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
+                    <nav className="flex flex-col items-center mt-32">
+                        <a href="/about" className="mobile-navbar-link mobile-navbar-link-text">About</a>
+                        <a href="/connect" className="mobile-navbar-link mobile-navbar-link-text">Connect</a>
+                        <a href="/event" className="mobile-navbar-link mobile-navbar-link-text">Events</a>
+                        <a href="/contact" className="mobile-navbar-link mobile-navbar-link-text">Contact</a>
                     </nav>
                 </div>
+            </div>
     );
 }
