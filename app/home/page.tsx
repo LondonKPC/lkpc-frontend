fdb8d686d9752fcd1e3fc98ea77356b31719291f
Nathaniel Park <nathanpark26@gmail.com>
2024-12-29 19:30:15 -0500

    Footer icon and horizontal scrollbar fix
    The icons were not hoverable nor clickable.
    On some devices, the vertical scrollbar caused the horizontal scrollbar to appear

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="h-full">
-      <div className="flex h-full">
+    <div className="h-full w-full">
+      <div className="flex h-full w-full">
         <div className="w-1/2 bg-slate-700 float-left text-center">
             Home...
         </div>
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -8,22 +8,22 @@ const Footer = () => {
     <div>
       <div className="bg-[#7a7c7b] w-full h-[50px] flex items-center justify-center">
         <div className="flex gap-4">
-          <a href="https://www.roblox.com/home" className="w-[24px] h-[24px] inline-block">
+          <a href="https://www.roblox.com/home" className="w-[24px] h-[24px] inline-block z-50">
             <img src={email.src} className="w-full h-full hover:opacity-30" />
           </a>
-          <a href="https://www.roblox.com/home" className="w-[24px] h-[24px] inline-block">
+          <a href="https://www.roblox.com/home" className="w-[24px] h-[24px] inline-block z-50">
             <img src={insta.src} className="w-full h-full hover:opacity-30" />
           </a>
-          <a href="https://www.roblox.com/home" className="w-[24px] h-[24px] inline-block">
+          <a href="https://www.roblox.com/home" className="w-[24px] h-[24px] inline-block z-50">
             <img src={disc.src} className="w-full h-full hover:opacity-30" />
           </a>
         </div>
       </div>
       <div className="flex w-full bg-[#18181a] py-14 flex-wrap">
-        <div className="px-8 flex-1 text-center">
+        <div className="flex-1 text-center">
           <p className="text-[20px] md:text-[24px] sm:text-[16px]">LondonKPC</p>
         </div>
-        <div className="px-8 flex-1 text-center">
+        <div className="flex-1 text-center">
           <p className="text-[20px] md:text-[24px] sm:text-[16px]">Service</p>
           <div>
             <p className="text-[10px] md:text-xl sm:text-lg">
