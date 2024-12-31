a81c53449cb8e8a702952c3dcde7e6b57876b057
Eunsung Kim <jeromekim0503@gmail.com>
2024-12-30 22:48:57 -0500

    add home-page card placeholders

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,27 +1,35 @@
 import React from "react";
 import HomeCard from "@/components/home_card/HomeCard";
+import background from "@/images/background.png";
 import box1 from "@/images/boxes/box1.jpg";
 import box2 from "@/images/boxes/box2.jpg";
 import box3 from "@/images/boxes/box3.jpg";
 import box4 from "@/images/boxes/box4.jpg";
 
-
 function Home() {
   return (
-    <div className="h-full">
+    <div className="h-full w-full flex justify-center">
       <div
-        className="flex justify-center items-center w-full h-full bg-cover md:bg-full bg-center"
-        //style={{ backgroundImage: `url(${churchBackground.src})` }}
+        className="flex justify-center bg-cover w-full md:bg-full bg-center"
+        style={{ backgroundImage: `url(${background.src})` }}
       >
         {/* Desktop */}
-        <div className="bg-white w-full h-full bg-opacity-50">
-          <div className="flex flex-col items-center justify-center my-52">
-            <h1 className="text-white text-4xl font-bold">Connect With Us</h1>
-            <div className="grid gap-2 lg:grid-cols-4 md:grid-cols-2">
-              <HomeCard imageSrc={box1} description="Gr. 7-12" />
-              <HomeCard imageSrc={box2} description="Gr. 7-12" />
-              <HomeCard imageSrc={box3} description="Gr. 7-12" />
-              <HomeCard imageSrc={box4} description="Gr. 7-12" />
+        <div className="w-[90%]">
+          <div className="flex flex-col items-center justify-center my-52 space-y-10">
+            <h1 className="text-black text-center text-4xl font-bold mb-10"> Welcome to LondonKPC EM</h1>
+            <div className="grid gap-5 lg:grid-cols-4 md:grid-cols-2 justify-center">
+              <HomeCard
+                imageSrc={box1}
+                title="About Us"
+                description="ioaj ieojsoeifja osejfaoi jfoiajf ojoaisjefoiajsoifjaois jefoiajes oijfeiojas oijoi joesjoije oijefoaijfioaefj ioajsef ioajoifjaoisjfoiaejs foaijoijfaoijaoij"
+              />
+              <HomeCard
+                imageSrc={box2}
+                title="Our Mission"
+                description="Gr. 7-12"
+              />
+              <HomeCard imageSrc={box3} title="Hello" description="Gr. 7-12" />
+              <HomeCard imageSrc={box4} title="Hello" description="Gr. 7-12" />
             </div>
           </div>
         </div>
--- components/home_card/HomeCard.tsx
+++ components/home_card/HomeCard.tsx
@@ -3,14 +3,16 @@ import Image, { StaticImageData } from 'next/image';
 
 interface HomeCardProps {
   imageSrc: StaticImageData;
+  title: string;
   description: string;
 }
 
-const HomeCard: React.FC<HomeCardProps> = ({ imageSrc, description }) => {
+const HomeCard: React.FC<HomeCardProps> = ({ imageSrc,title, description }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <Image src={imageSrc} alt="Card Image" className="w-full" />
-      <div className="px-4 py-4">
+    <div className="max-w-sm rounded-lg shadow-lg bg-white">
+      <Image src={imageSrc} alt="Card Image" className="w-full rounded-t-lg" />
+      <div className="px-4 py-4 ">
+        <h1 className="text-gray-900 font-bold text-xl">{title}</h1>
         <p className="text-gray-700 text-base">{description}</p>
       </div>
     </div>
