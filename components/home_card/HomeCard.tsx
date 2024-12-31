1f7039794d2b84356d4d00ec9cf88d663e5afdee
Eunsung Kim <127615531+jerpyk@users.noreply.github.com>
2024-12-31 15:02:36 -0500

    Update components/home_card/HomeCard.tsx
    Co-authored-by: Brian Park <96549640+Cheekane@users.noreply.github.com>

--- components/home_card/HomeCard.tsx
+++ components/home_card/HomeCard.tsx
@@ -11,7 +11,7 @@ const HomeCard: React.FC<HomeCardProps> = ({ imageSrc,title, description }) => {
   return (
     <div className="max-w-sm rounded-lg shadow-lg bg-white">
       <Image src={imageSrc} alt="Card Image" className="w-full rounded-t-lg" />
-      <div className="px-4 py-4 ">
+      <div className="px-4 py-4">
         <h1 className="text-gray-900 font-bold text-xl">{title}</h1>
         <p className="text-gray-700 text-base">{description}</p>
       </div>
