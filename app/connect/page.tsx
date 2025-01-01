7d4184e1d97ada10a6d765d6d6647b17f21dec35
Brian Park <brianpark1101@gmail.com>
2024-12-31 22:51:01 -0500

    Fixed screen handling

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -5,18 +5,20 @@ import ConnectCard from '@/components/connect_card/ConnectCard';
 
 export default function Connect() {
   return (
-    <div className="flex justify-center items-center w-full min-h-full bg-cover md:bg-full bg-center" style={{ backgroundImage: `url(${churchBackground.src})` }}>
-        {/* Desktop */}
-        <div className="bg-white w-full h-screen bg-opacity-50">
-          <div className="flex flex-col items-center justify-center my-52">
-            <h1 className="text-center text-white text-4xl font-bold">Connect With Us</h1>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2">
-              <ConnectCard groupName="HI-C" groupDescription="Gr. 7-12"/>
-              <ConnectCard groupName="CU" groupDescription="College &  University" />
-              <ConnectCard groupName="Adults" groupDescription="Graduates+" />
+      <div className="bg-gray-100">
+          <div className="flex justify-center items-center w-full min-h-screen bg-cover md:bg-full bg-center" style={{ backgroundImage: `url(${churchBackground.src})` }}>
+              {/* Desktop */}
+              <div className="bg-white w-full min-h-screen bg-opacity-50">
+                  <div className="flex flex-col items-center justify-center my-52">
+                      <h1 className="text-center text-white text-4xl font-bold">Connect With Us</h1>
+                      <div className="grid lg:grid-cols-3 md:grid-cols-2">
+                          <ConnectCard groupName="HI-C" groupDescription="Gr. 7-12"/>
+                          <ConnectCard groupName="CU" groupDescription="College &  University" />
+                          <ConnectCard groupName="Adults" groupDescription="Graduates+" />
+                      </div>
+                  </div>
+              </div>
           </div>
-        </div>
       </div>
-    </div>
   );
 }
