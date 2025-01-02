65ffeb787877ac3c3cde9408803956ac908e1e37
Brian Park <brianpark1101@gmail.com>
2025-01-02 17:48:02 -0500

    Update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+{/* TODO: Eventually we will make contact card components to add and change different contact information. */}
+
 const Contact = () => {
   return (
     <div className="w-full min-h-full lg:full bg-white flex flex-col items-center mb-16">
-      <div className="w-[95%] bg-[#3c98bd] mt-16 border-solid border-4 border-black">
+      <div className="w-[95%] bg-gray-200 mt-20 border-solid border-[1px] border-gray-500 shadow-gray-500 shadow-sm">
         <iframe 
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12368.291131761114!2d-81.32164932840173!3d42.988210509180796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ef017ef815c35%3A0xbebac0814ea204b3!2sGateway%20Church!5e0!3m2!1sen!2sca!4v1735675865678!5m2!1sen!2sca" 
           className="w-full h-[50vh]" 
@@ -15,16 +17,16 @@ const Contact = () => {
           </p>
         </div>
       </div>
-      <div className="mt-8 w-[50%] sm:w-[30%]">
-        <p className="text-[#3a719e] text-2xl">
+      <div className="mt-8 w-3/4 md:w-1/3">
+        <p className="text-black text-2xl font-bold">
           Email
         </p>
         <p className="text-black text-lg">
           Contact us at info@example.com for more details.
         </p>
       </div>
-      <div className="mt-8 w-[50%] sm:w-[30%]">
-        <p className="text-[#3a719e] text-2xl py-2">
+      <div className="mt-8 w-3/4 md:w-1/3">
+        <p className="text-black text-2xl font-bold">
           Phone
         </p>
         <p className="text-black text-lg">
